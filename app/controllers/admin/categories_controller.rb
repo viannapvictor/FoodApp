0f0a9cd49b9e8d421d536f4104831198f5aafb31
Victor Vianna <vitorvianna27@gmail.com>
2025-01-26 16:09:10 -0300

    :sparkles:[FEAT]: Image Upload

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -38,7 +38,7 @@ class Admin::CategoriesController < AdminController
   def update
     respond_to do |format|
       if @admin_category.update(admin_category_params)
-        format.html { redirect_to @admin_category, notice: "Category was successfully updated." }
+        format.html { redirect_to admin_category_url(@admin_category), notice: "Category was successfully updated." }
         format.json { render :show, status: :ok, location: @admin_category }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -65,6 +65,6 @@ class Admin::CategoriesController < AdminController
 
     # Only allow a list of trusted parameters through.
     def admin_category_params
-      params.expect(category: [ :name, :description ])
+      params.require(:category).permit(:name, :description, :image)
     end
 end
