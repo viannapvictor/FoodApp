5829d9d7fb4da6390cf84245745c767652a4a7f3
Victor Vianna <vitorvianna27@gmail.com>
2025-01-26 23:12:47 -0300

    :sparkles:[FEAT]: Stock Control feature

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :category
+  has_many :stocks
   has_many_attached :images do |attachable|
     attachable.variant :thumb, resize_to_limit: [ 50, 50 ]
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_26_191416) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_27_013603) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -72,7 +72,17 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_26_191416) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  create_table "stocks", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.integer "amount"
+    t.string "size"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_stocks_on_product_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "products", "categories"
+  add_foreign_key "stocks", "products"
 end
